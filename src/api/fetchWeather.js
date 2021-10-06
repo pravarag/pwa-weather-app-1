ea86fde362cd2d52febdfa94c895021314082a0a
Pawel Borkar <paawel@pm.me>
2021-10-06 16:15:35 +0530

    Base Template

---
+++ src/api/fetchWeather.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const fetchWeather = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default fetchWeather
+
+
+// http://maps.openweathermap.org/maps/2.0/weather/{op}/{z}/{x}/{y}?appid={API key}
